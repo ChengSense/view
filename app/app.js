68bde583f2c319c9b7e92581507ff5d5312dbac1
chengdongliang <chengdongliang@51tuodao.com>
2017-11-14 19:01:18 +0800

    更新官网

--- app/app.js
+++ app/app.js
@@ -21,7 +21,7 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + "/index.html");
 })
 
-var server = app.listen(8081, function () {
+var server = app.listen(80, function () {
 
   var host = server.address().address
   var port = server.address().port
