a28285986010040290d2673cf135bf7c3adb5adc
chengdongliang <chengdongliang@51tuodao.com>
2017-11-22 18:35:00 +0800

    提交

--- app/app/chapter.js
+++ app/app/chapter.js
@@ -1,26 +1,21 @@
-var form = new view({
-    "@view": 
-    "<div id='control'>\
-        <div> {{ msg }} </div>\
-        <input class='form-control' value='{{msg}}' />\
-    </div>",
-    model: {
-        msg: "输入点什么?"
-    }
-});
+$.get("/chapter.html", function (data) {
 
-var chapter = new view({
-    view: "view",
-    model: {
-        chapters: "",
-        form: form
-    },
-    controller(model) {
-        $.get("/chapter.html", function (data) {
-            model.chapters = data;
-            $('pre code').each(function (i, block) {
-                hljs.highlightBlock(block);
-            });
-        });
-    }
+    new view({
+        view: "view",
+        model: {
+            chapters: data
+        }
+    });
+
+    new view({
+        view: "div#input",
+        model: {
+            text: "输入点什么?"
+        }
+    });
+
+    $('pre code').each(function (i, block) {
+        hljs.highlightBlock(block);
+    });
+
 });
